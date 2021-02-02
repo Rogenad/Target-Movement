5db54fd5ee599abc4d4f3475a5f23af77bd7dceb
Rogenad <roman.zhukov.2002@bk.ru>
2021-02-03 01:14:05 +0300

    Final( I mean I hope)

--- Assets/TargetMoving.cs
+++ Assets/TargetMoving.cs
@@ -3,9 +3,18 @@
 public class TargetMoving : MonoBehaviour
 {
     public Camera mainCamera;
-    public float speed = 2.5f;
+    public float speed = 1.0f;
+    
+    private Vector3 _start = new Vector3(0, 6, 0);
+    private Vector3 _target = new Vector3(10, 6, 0);
+    private float _distance;
+    private float _startTime;
 
-    private Vector3 _target = new Vector3(0.6f,6,-0.67f);
+    private void Start()
+    {
+        _distance = Vector3.Distance(_start, _target);
+        _startTime = Time.time;
+    }
 
     // Update is called once per frame
     private void Update()
@@ -17,10 +26,17 @@ public class TargetMoving : MonoBehaviour
             RaycastHit _hit;
             if (Physics.Raycast(ray, out _hit, Mathf.Infinity))
             {
+                _start = transform.position;
                 _target = _hit.point;
+                _distance = Vector3.Distance(_start, _target);
+                _startTime = Time.time;
             }
         }
+        
+        var distPast = (Time.time - _startTime) * speed;
+        var fractionOfDistance = distPast / _distance;
 
-        transform.position = Vector3.Lerp(transform.position, _target, Time.deltaTime * speed);
+        // Set our position as a fraction of the distance between the markers.
+        transform.position = Vector3.Lerp(_start, _target, fractionOfDistance);
     }
 }
