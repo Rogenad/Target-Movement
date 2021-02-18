9fa7fc039700acbed292d4054ece8e0423be2a43
Rogenad <roman.zhukov.2002@bk.ru>
2021-02-19 00:25:13 +0300

    Level System Added

--- Assets/Bonuses/BonusRandomizer.cs
+++ Assets/Bonuses/BonusRandomizer.cs
@@ -10,10 +10,14 @@ namespace Bonuses
         [SerializeField] 
         private List<Button> _buttons = new List<Button>();
 
-        public void GetRandomBonuses()
+        private void OnEnable()
+        {
+            GetRandomBonuses();
+        }
+
+        private void GetRandomBonuses()
         {
             var bonuses = Bonus.BonusList;
-            Debug.Log(bonuses.Count);
             for (var i = 0; i < 3; i++)
             {
                 var randomBonus = bonuses[Random.Range(0, bonuses.Count)];
