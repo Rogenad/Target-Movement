1c9d62b7c1cf843d10d68272e0bc9e1e3e5bf62f
Rogenad <roman.zhukov.2002@bk.ru>
2021-02-20 12:08:34 +0300

    Added Joystick and logic to it

---
+++ Assets/Player/PlayerMovementControlller.cs
@@ -0,0 +1,26 @@
+﻿using UI.Joystick;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerMovementControlller : MonoBehaviour
+    {
+        [SerializeField]
+        private Rigidbody _rigidBody;
+        [SerializeField]
+        private float _speed;
+        [SerializeField]
+        private JoystickBehaviour _joystick;
+
+        private void Update()
+        {
+            var direction = new Vector3(_joystick.InputDirection.x, 0, _joystick.InputDirection.y);
+            _rigidBody.velocity = direction * _speed;
+            if (!direction.Equals(Vector3.zero))
+            {
+                transform.rotation = Quaternion.LookRotation(_rigidBody.velocity);
+            }
+        }
+
+    }
+}
---
+++ Assets/UI/Joystick/JoystickBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace UI.Joystick
+{
+    public class JoystickBehaviour : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+    {
+        [SerializeField]
+        private Image _joystickBackgroundImage;
+        [SerializeField]
+        private Image _joystickImage;
+        [SerializeField]
+        private float _offset;
+        public Vector2 InputDirection { get; private set; }
+
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            OnDrag(eventData);
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            InputDirection = Vector2.zero;
+            _joystickImage.rectTransform.anchoredPosition = InputDirection;
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            var sizeDelta = _joystickBackgroundImage.rectTransform.rect.size;
+            var backgroundImageSizeX = sizeDelta.x;
+            var backgroundImageSizeY = sizeDelta.y;
+
+            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(_joystickBackgroundImage.rectTransform,
+                eventData.position, eventData.pressEventCamera, out var position))
+            {
+                position.x /= backgroundImageSizeX;
+                position.y /= backgroundImageSizeY;
+
+                InputDirection = new Vector2(position.x, position.y);
+                InputDirection = InputDirection.magnitude > 1 ? InputDirection.normalized : InputDirection;
+
+                _joystickImage.rectTransform.anchoredPosition = new 
+                    Vector2(InputDirection.x * (backgroundImageSizeX / _offset),
+                        InputDirection.y * (backgroundImageSizeY / _offset));
+            }
+        }
+    }
+}
